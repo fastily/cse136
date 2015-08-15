d05b17ebed84da5a84a3aefbbdc949c110a7d7ef
nardecky <nardecky@ucsd.edu>
2015-08-15 13:42:50 -0700

    Added ICapeReview repository and POCO for cape_review and grade_change

---
+++ Solution136/POCO/CapeReview.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POCO
+{
+    public class CapeReview
+    {
+        public int CapeId { get; set; }
+        public string InstructorName { get; set; }
+        public string CourseName { get; set; }
+        public int InstructorRating { get; set; }
+        public string summary { get; set; }
+        public int CourseRating { get; set; }
+    }
+}
---
+++ Solution136/POCO/GradeChange.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POCO
+{
+    public class GradeChange
+    {
+        public int GradeChangeId { get; set; }
+        public int student_id { get; set; }
+        public int schedule_id { get; set; }
+        public Nullable<bool> approved { get; set; }
+        public int course_id { get; set; }
+    }
+}
