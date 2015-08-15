ea2d8c215312acd5b9c90ffb928839f2bacee2df
bhany <bhany8@gmail.com>
2015-08-15 16:54:26 -0700

    TaRepository implemented

---
+++ Solution136/POCO/Instructor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POCO
+{
+    public class Instructor
+    {
+        public int instructor_id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string title { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
