e56d534671974f5be642c49e440eb3b9717faa3e
nardecky <nardecky@ucsd.edu>
2015-08-15 14:21:55 -0700

    Updating css and Authorization controller in WebApi

--- Solution136/WebApi/Controllers/AdminController.cs
+++ Solution136/WebApi/Controllers/AdminController.cs
@@ -1,10 +1,29 @@
 ﻿namespace WebApi.Controllers
 {
+    using System.Collections.Generic;
     using System.Web.Http;
     using POCO;
+    using Repository;
+    using Service;
+    using IRepository;
 
     public class AdminController : ApiController
     {
+        private cse136Entities _cse136Entities;
+
+        //default constructor for runtime use
+        public AdminController()
+        {
+            _cse136Entities = new cse136Entities();
+        }
+
+        //overloaded constructor for dependency injection
+        //used for testing
+        public AdminController(cse136Entities Cse136Entities)
+        {
+            _cse136Entities = Cse136Entities;
+        }
+
         [HttpGet]
         public Admin GetAdminInfo(int adminId)
         {
