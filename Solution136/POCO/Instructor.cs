1790497eed6fae31199a585f97dc443349c40f06
nardecky <nardecky@ucsd.edu>
2015-08-15 17:31:17 -0700

    merge conflict add

---
+++ Solution136/POCO/Instructor.cs
@@ -0,0 +1,12 @@
+﻿namespace POCO
+{
+    public class Instructor
+    {
+        public int InstructorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
