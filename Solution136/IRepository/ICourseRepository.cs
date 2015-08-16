4818e674ef143d3f88218c125fa82cfe98acf038
nardecky <nardecky@ucsd.edu>
2015-08-16 13:02:49 -0700

    Adding EnrollmentRepository. Not sure if we will use it or not.

--- Solution136/IRepository/ICourseRepository.cs
+++ Solution136/IRepository/ICourseRepository.cs
@@ -23,6 +23,8 @@
 
         void RemovePreReqFromCourse(int courseId, int preReqToRemoveCourseId, ref List<string> errors);
 
+        //void ApprovePreReqOvverid()
+
         List<Course> GetAllPreReqs(int courseId, ref List<string> errors);
     }
 }
