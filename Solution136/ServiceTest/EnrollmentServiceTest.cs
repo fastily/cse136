72e9f49ff83015deb97a7505e08a029d6b0c2303
bhany <bhany8@gmail.com>
2015-08-22 09:45:14 -0700

    ScheduleServiceTest, EnrollmentServiceTest

--- Solution136/ServiceTest/EnrollmentServiceTest.cs
+++ Solution136/ServiceTest/EnrollmentServiceTest.cs
@@ -12,5 +12,112 @@
     [TestClass]
     public class EnrollmentServiceTest
     {
+       
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddEnrollmentErrorTest1() // :  studentId  == null
+        {
+            //// Arranage
+            var errors = new List<string>();
+            var mockRepository = new Mock<IEnrollmentRepository>();
+            var enrollmentService = new EnrollmentService(mockRepository.Object);
+
+            //// Act
+            enrollmentService.AddEnrollment(string.Empty, 0,ref errors);
+
+            //// Assert cant be null course object
+            Assert.AreEqual(1, errors.Count);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddEnrollmentErrorTest1() // :  scheduleId  == null
+        {
+            //// Arranage
+            var errors = new List<string>();
+            var mockRepository = new Mock<IEnrollmentRepository>();
+            var enrollmentService = new EnrollmentService(mockRepository.Object);
+            int? nullInt = null;
+            //// Act
+            enrollmentService.AddEnrollment("1", (int)nullInt, ref errors);
+
+            //// Assert cant be null course object
+            Assert.AreEqual(1, errors.Count);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddEnrollmentErrorTest1() // :  scheduleId  == null
+        {
+            //// Arranage
+            var errors = new List<string>();
+            var mockRepository = new Mock<IEnrollmentRepository>();
+            var enrollmentService = new EnrollmentService(mockRepository.Object);
+            int? nullInt = null;
+            //// Act
+            enrollmentService.AddEnrollment("1", (int)nullInt, ref errors);
+
+            //// Assert cant be null course object
+            Assert.AreEqual(1, errors.Count);
+        }
+
+        /* how to check error for duplicate insertion - do we even need this ?
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddEnrollmentErrorTest1() // :  scheduleId  == null
+        {
+            //// Arranage
+            var errors = new List<string>();
+            var mockRepository = new Mock<IEnrollmentRepository>();
+            var enrollmentService = new EnrollmentService(mockRepository.Object);
+            int? nullInt = null;
+            //// Act
+            enrollmentService.AddEnrollment("1", (int)nullInt, ref errors);
+
+            //// Assert cant be null course object
+            Assert.AreEqual(1, errors.Count);
+        }
+         */
+
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void RemoveEnrollmentErrorTest() // course == null
+        {
+            //// Arrange
+            var errors = new List<string>();
+
+            var mockRepository = new Mock<IEnrollmentRepository>();
+            var enrollmentService = new EnrollmentService(mockRepository.Object);
+            
+            //// Act
+            enrollmentService.RemoveEnrollment(string.Empty, 1, ref errors);
+
+            //// Assert course id cannot be null
+            Assert.AreEqual(1, errors.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void RemoveEnrollmentErrorTest() // course == null
+        {
+            //// Arrange
+            var errors = new List<string>();
+
+            var mockRepository = new Mock<IEnrollmentRepository>();
+            var enrollmentService = new EnrollmentService(mockRepository.Object);
+
+            int? nullInt = null;
+            //// Act
+            enrollmentService.AddEnrollment("1", (int)nullInt, ref errors);
+
+            //// Assert course id cannot be null
+            Assert.AreEqual(1, errors.Count);
+        }
+
+
     }
 }
--- Solution136/ServiceTest/ScheduleServiceTest.cs
+++ Solution136/ServiceTest/ScheduleServiceTest.cs
@@ -12,5 +12,76 @@
     [TestClass]
     public class ScheduleServiceTest
     {
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddCourseToScheduleErrorTest1() // :  schedule  == null
+        {
+            //// Arranage
+            var errors = new List<string>();
+            var mockRepository = new Mock<IScheduleRepository>();
+            var scheduleService = new ScheduleService(mockRepository.Object);
+            int? nullInt = null;
+            //// Act
+            scheduleService.AddCourseToSchedule(null, 1, "2", "3", ref errors);
+
+            //// Assert cant be null course object
+            Assert.AreEqual(1, errors.Count);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddCourseToScheduleErrorTest2() // :  dayId  == null
+        {
+            //// Arranage
+            var errors = new List<string>();
+            var mockRepository = new Mock<IScheduleRepository>();
+            var scheduleService = new ScheduleService(mockRepository.Object);
+            var schedule = new Schedule { };
+
+            //// Act
+            scheduleService.AddCourseToSchedule(schedule, 1, string.Empty, "3", ref errors);
+
+            //// Assert cant be null course object
+            Assert.AreEqual(1, errors.Count);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddCourseToScheduleErrorTest3() // :  timeID  == null
+        {
+            //// Arranage
+            var errors = new List<string>();
+            var mockRepository = new Mock<IScheduleRepository>();
+            var scheduleService = new ScheduleService(mockRepository.Object);
+            var schedule = new Schedule { };
+            //// Act
+            scheduleService.AddCourseToSchedule(schedule, 1, "2", string.Empty, ref errors);
+
+            //// Assert cant be null course object
+            Assert.AreEqual(1, errors.Count);
+        }
+
+
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void RemoveCourseFromScheduleErrorTest() // course == null
+        {
+            //// Arrange
+            var errors = new List<string>();
+
+            var mockRepository = new Mock<IScheduleRepository>();
+            var scheduleService = new ScheduleService(mockRepository.Object);
+
+            //// Act
+            scheduleService.RemoveCourseFromSchedule(string.Empty, ref errors);
+
+            //// Assert course id cannot be null
+            Assert.AreEqual(1, errors.Count);
+        }
     }
 }
