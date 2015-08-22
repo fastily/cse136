44eada1e5f9bf03e7dc2b5679427826bc701440c
lalacson <lalacson@ucsd.edu>
2015-08-22 16:26:19 -0700

    added ScheduleController

---
+++ Solution136/WebApi/Controllers/ScheduleController.cs
@@ -0,0 +1,65 @@
+﻿namespace WebApi.Controllers
+{
+    using System.Collections.Generic;
+    using System.Web.Http;
+    using IRepository;
+    using POCO;
+    using Repository;
+    using Service;
+
+    public class ScheduleController : ApiController
+    {
+        private cse136Entities entities;
+
+        public ScheduleController()
+        {
+            this.entities = new cse136Entities();
+        }
+
+        public ScheduleController(cse136Entities entities)
+        {
+            this.entities = entities;
+        }
+
+        [HttpGet]
+        public List<Schedule> GetScheduleList(string year, string quarter)
+        {
+            var service = new ScheduleService(new ScheduleRepository(this.entities));
+            var errors = new List<string>();
+
+            return service.GetScheduleList(year, quarter, ref errors);
+        }
+
+        [HttpPost]
+        public string AddCourseToSchedule(Schedule schedule, int instructorId, string dayId, string timeId)
+        {
+            var errors = new List<string>();
+            var repository = new ScheduleRepository(this.entities);
+            var service = new ScheduleService(repository);
+            service.AddCourseToSchedule(schedule, instructorId, dayId, timeId, ref errors);
+
+            if (errors.Count == 0)
+            {
+                return "ok";
+            }
+
+            return "error";
+        }
+
+        [HttpPost]
+        public string DeleteCourseFromSchedule(string id)
+        {
+            var errors = new List<string>();
+            var repository = new ScheduleRepository(this.entities);
+            var service = new ScheduleService(repository);
+            service.RemoveCourseFromSchedule(id, ref errors);
+
+            if (errors.Count == 0)
+            {
+                return "ok";
+            }
+
+            return "error";
+        }
+    }
+}
