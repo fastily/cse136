9e897950e1ecf3b5a20b90021008e9a9ed5ae230
fastily <thisisalecwu@gmail.com>
2015-08-23 14:22:36 -0700

    finished schueduleservice tests

--- Solution136/ServiceTest/ScheduleServiceTest.cs
+++ Solution136/ServiceTest/ScheduleServiceTest.cs
@@ -77,5 +77,68 @@
             //// Assert course id cannot be null
             Assert.AreEqual(1, errors.Count);
         }
+
+
+        [TestMethod]
+        public void GetScheduleListTest()
+        {
+            //// Arrange
+            var errors = new List<string>();
+
+            Mock<IScheduleRepository> mockRepository = new Mock<IScheduleRepository>();
+            ScheduleService schedservice = new ScheduleService(mockRepository.Object);
+
+            List<Schedule> crl = new List<Schedule>();
+            crl.Add(new Schedule { ScheduleId = 99, Year = "1000", Quarter = "Winter", Session = "2" });
+
+            mockRepository.Setup(x => x.GetScheduleList("1000", "Winter", ref errors)).Returns(crl);
+
+            //// Act
+            Schedule temp = schedservice.GetScheduleList("1000", "Winter", ref errors)[0];
+
+            //// Assert
+            Assert.AreEqual(99, temp.ScheduleId);
+            ////Assert.AreEqual(3, rating);
+        }
+
+        [TestMethod]
+        public void AddCourseScheduleTest()
+        {
+            //// Arranage
+            var errors = new List<string>();
+
+            Mock<IScheduleRepository> mockRepository = new Mock<IScheduleRepository>();
+            ScheduleService iserv = new ScheduleService(mockRepository.Object);
+
+            Schedule s = new Schedule { ScheduleId = 99, Year = "1000", Quarter = "Winter", Session = "2" };
+
+            mockRepository.Setup(x => x.AddCourseToSchedule(s, 99, 1, 1, ref errors));
+
+            //// Act
+            iserv.AddCourseToSchedule(s, 99, "1", "1", ref errors);
+
+            //// Assert
+            mockRepository.Verify(x => x.AddCourseToSchedule(s, 99, 1, 1, ref errors), Times.Once());
+        }
+
+        [TestMethod]
+        public void RemoveCourseFromScheduleTest()
+        {
+            //// Arranage
+            var errors = new List<string>();
+
+            Mock<IScheduleRepository> mockRepository = new Mock<IScheduleRepository>();
+            ScheduleService iserv = new ScheduleService(mockRepository.Object);
+
+            Schedule s = new Schedule { ScheduleId = 99, Year = "1000", Quarter = "Winter", Session = "2" };
+
+            mockRepository.Setup(x => x.RemoveCourseFromSchedule(99, ref errors));
+
+            //// Act
+            iserv.RemoveCourseFromSchedule("99", ref errors);
+
+            //// Assert
+            mockRepository.Verify(x => x.RemoveCourseFromSchedule(99, ref errors), Times.Once());
+        }
     }
 }
