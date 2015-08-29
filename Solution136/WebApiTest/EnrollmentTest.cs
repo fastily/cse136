7d488af2b072f3caeb721e0d3029f5bf86b588ee
bhany <bhany8@gmail.com>
2015-08-29 14:37:42 -0700

    nada

---
+++ Solution136/WebApiTest/EnrollmentTest.cs
@@ -0,0 +1,30 @@
+﻿namespace WebApiTest
+{
+    using System.Diagnostics;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using POCO;
+    using WebApi.Controllers;
+
+    /// <summary>
+    /// Enrollment test cases
+    /// </summary>
+    [TestClass]
+    public class EnrollmentTest
+    {
+        [TestMethod]
+        public void EnrollmentGetTest()
+        {
+            var enrollmentController = new EnrollmentController();
+            var  = enrollmentController.AddEnrollment("1", 1);
+            Assert.AreEqual("1", login.Id);
+        }
+        
+        [TestMethod]
+        public void EnrollmentPostTest()
+        {
+            var enrollmentController = new EnrollmentController();
+            var login = enrollmentController.RemoveEnrollment("1", 1);
+            Assert.AreEqual("1", login.Id);
+        }
+    }
+}
