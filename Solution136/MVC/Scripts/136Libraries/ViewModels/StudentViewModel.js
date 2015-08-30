fb6d65fbd42425675fd0d063a2052c0a700b2670
nardecky <nardecky@ucsd.edu>
2015-08-29 21:26:47 -0700

    fixing some minor stuff

--- Solution136/MVC/Scripts/136Libraries/ViewModels/StudentViewModel.js
+++ Solution136/MVC/Scripts/136Libraries/ViewModels/StudentViewModel.js
@@ -2,31 +2,30 @@
 
     var StudentModelObj = new StudentModel();
     var self = this;
-    //var initialBind = true;
+    var initialBind = true;
     var studentListViewModel = ko.observableArray();
 
-    // this.Initialize = function() {
+    this.Initialize = function() {
 
-    var viewModel = {
-        id: ko.observable("A0000111"),
-        first: ko.observable("Bruce"),
-        last: ko.observable("Wayne"),
-        email: ko.observable("bwayne@ucsd.edu"),
-        password: ko.observable("password"),
-        add: function (data) {
-            self.CreateStudent(data);
-        }
-        //   };
+        var viewModel = {
+            id: ko.observable("A0000111"),
+            first: ko.observable("Bruce"),
+            last: ko.observable("Wayne"),
+            email: ko.observable("bwayne@ucsd.edu"),
+            password: ko.observable("password"),
+            add: function (data) {
+                self.CreateStudent(data);
+            }
+        };
 
-        //  ko.applyBindings(viewModel, document.getElementById("divStudentContent"));
+        ko.applyBindings(viewModel, document.getElementById("divStudent"));
     };
 
     this.Initialize2 = function () {
         ko.applyBindings(viewModel, document.getElementById("divStudentContent"));
     };
 
-
-    this.CreateStudent = function (data) {
+    this.CreateStudent = function(data) {
         var model = {
             StudentId: data.id(),
             FirstName: data.first(),
@@ -35,7 +34,7 @@
             Password: data.password()
         }
 
-        StudentModelObj.Create(model, function (result) {
+        StudentModelObj.Create(model, function(result) {
             if (result == "ok") {
                 alert("Create student successful");
             } else {
@@ -45,9 +44,9 @@
 
     };
 
-    this.GetAll = function () {
+    this.GetAll = function() {
 
-        StudentModelObj.GetAll(function (studentList) {
+        StudentModelObj.GetAll(function(studentList) {
             studentListViewModel.removeAll();
 
             for (var i = 0; i < studentList.length; i++) {
@@ -69,36 +68,19 @@
     this.GetDetail = function (id) {
 
         StudentModelObj.GetDetail(id, function (result) {
-
+            
             var student = {
-                id: ko.observable(result.StudentId),
-                first: ko.observable(result.FirstName),
-                last: ko.observable(result.LastName),
-                email: ko.observable(result.Email),
-                password: ko.oberervable(result.Password),
-                update: function () {
-                    self.UpdateStudent(this);
-                }
+                id: result.StudentId,
+                first: result.FirstName,
+                last: result.LastName,
+                email: result.Email,
+                password: result.Password
             };
 
-            ko.applyBindings(student, document.getElementById("divStudentContent"));
+            if (initialBind) {
+                ko.applyBindings({ viewModel: student }, document.getElementById("divStudentContent"));
+            }
         });
-    };
-
-    this.UpdateStudent = function (viewModel) {
-        // convert the viewModel to same structure as PLAdmin model (presentation layer model)
-        var stduentData = {
-            first: ko.observable(result.FirstName),
-            last: ko.observable(result.LastName),
-            email: ko.observable(result.email),
-            password: ko.observable(result.password),
-            id: result.Id,
-        };
-
-        StduentModelObj.Update(stduentData, function (message) {
-            $('#divMessage').html(message);
-        });
-
     };
 
     ko.bindingHandlers.DeleteStudent = {
@@ -106,7 +88,7 @@
             $(element).click(function () {
                 var id = viewModel.id;
 
-                StudentModelObj.Delete(id, function (result) {
+                StudentModelObj.Delete(id, function(result) {
                     if (result != "ok") {
                         alert("Error occurred");
                     } else {
@@ -116,5 +98,4 @@
             });
         }
     }
-
 }
